bb2447de9f263211b8feba35ceb94910be8a1bcd
Eric <hayter.eric@gmail.com>
2024-03-30 11:33:17 -0400

    working on creating a room landing page for a specific room

--- hotel_system/hotel_system/views.py
+++ hotel_system/hotel_system/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # this is a python file defining all of the different pages
+from .models import Room, Amenity
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,14 @@ def index(request):
 
 def rooms(request):
     return render(request, "rooms.html")
+
+# page for a specific room
+def room(request, room_id):
+    try:
+        room_info = Room.objects.get(pk=room_id)
+    except Room.DoesNotExist:
+        raise Http404(f"Room id: {room_id}, does not exist")
+    return render(request, "room.html", {"room_info": room_info}) 
+
+
+    
